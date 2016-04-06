6348d152aa070c6fee86411b8cecee5caa1ae7a6
Steven Graboski <sgrabosk@gmail.com>
2016-04-06 19:52:20 -0400

    Changed file check to a synchronous one (good tip Walt)

--- customer/customer.js
+++ customer/customer.js
@@ -4,40 +4,43 @@ fs = require('fs');
 // menu file
 var menu = "../owner/menu.txt";
 
-// menu
-fs.readFile(menu, 'utf8', function(err, data){
-  if (!err) {
-    var menuArr = data.split('\n');
-    menuArr = eval(menuArr);
-    for (var i = 0; i < menuArr.length; i++) {
-      if (i != menuArr.length - 1) {
-        menuArr[i] = eval(menuArr[i])
-      }
-      else {
-        menuArr.splice(i, 1);
-      }
-    }
-    console.log(menuArr);
+// List
+var List = function(name,address,item,quantity){
+  this.name = name;
+  this.address = address;
+  this.item = item;
+  this.quantity = quantity;
+};
+
+// menu: grab file synchronously (thanks walt)
+// then make an array we'll reference in the prompt
+var menuOrig = fs.readFileSync(menu, 'utf8');
+
+// split the string
+var menuArr = menuOrig.split('\n');
+
+// eval string
+menuArr = eval(menuArr);
+
+// eval stringed arrays within the new array
+for (var i = 0; i < menuArr.length; i++) {
+  if (i != menuArr.length - 1) {
+    menuArr[i] = eval(menuArr[i])
   }
-})
+  // removes newline orphan
+  else {
+    menuArr.splice(i, 1);
+  }
+}
+// debug check
+console.log(menuArr);
 
-// var List = function(name,address,item,quantity){
-//   this.name = name;
-//   this.address = address;
-//   this.item = item;
-//   this.quantity = quantity;
-// };
+prompt.start();
 
-// prompt.start();
+prompt.get(['customer', 'address', 'item','quantity'], function (err, result) {
 
-// prompt.get(['customer', 'address', 'item','quantity'], function (err, result) {
-//   // console.log('Command-line input received:');
-//   // console.log('  name: ' + result.name);
-//   // console.log('  price: ' + result.price);
-//   // console.log('  category: ' + result.category);
+  var order = new List(result.customer,result.address,result.item,result.quantity);
 
-//   var order = new List(result.customer,result.address,result.item,result.quantity);
-
-//   console.log('Customer Order:');
-//   console.log(order);
-// });
+  console.log('Customer Order:');
+  console.log(order);
+});
