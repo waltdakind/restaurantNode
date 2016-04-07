0a46c1739e3a14dd6dfc548c3ca662ebf2ca6975
Steven Graboski <sgrabosk@gmail.com>
2016-04-06 20:54:15 -0400

    Comments added to owner.js

--- owner/owner.js
+++ owner/owner.js
@@ -23,12 +23,17 @@ var Item = function(dish, price, category) {
 
 // prompt time
 prompt.start();
+// start prompt
 prompt.get(['dish', 'price', 'category'], function(err, result) {
+	// if no error
 	if (!err) {
+		// make a new menu Item
 		var menuItem = new Item(result.dish, result.price, result.category);
+		// log the results to console
 		console.log("Menu Item: " + menuItem.dish);
 		console.log("Price: " + menuItem.price);
 		console.log("Category: " + menuItem.category);
+		// add info as an array on a new line in menu.txt
 		appender("['"+ menuItem.dish + "','" + menuItem.price + "','" + menuItem.category + "']\n");
 	}
 })
